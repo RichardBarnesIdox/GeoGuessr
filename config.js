894d2526f92a3619a492a18633ce6fdeae2ea219
Richard Barnes <richard.barnes@idoxgroup.com>
2026-04-14 17:51:02 +0100

    config back in

---
+++ config.js
@@ -0,0 +1,4 @@
+window.APP_CONFIG = {
+  googleMapsApiKey: "AIzaSyCNtf7SFY-L10zoik8Bn9Crw_T6NwmTb4s",
+  leaderboardDatabaseUrl: "https://geoguessr-bc28a-default-rtdb.europe-west1.firebasedatabase.app"
+};
